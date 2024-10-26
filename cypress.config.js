095c7c2b9fc86bfc8b4ff9f0d7abafa5e0493074
VitoriaBarr <vitoria.barrocas2.vmdsb@gmail.com>
2024-10-26 19:44:19 -0300

    testando

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("cypress");
+const {
+  beforeRunHook,
+  afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   e2e: {
+    baseUrl: "https://automationexercise.com",
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
